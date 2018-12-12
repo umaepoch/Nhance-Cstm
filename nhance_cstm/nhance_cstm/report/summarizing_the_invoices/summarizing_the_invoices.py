656d3eb35cef8bc450ab511283050d483352eb93
umaepoch <umag@epochconsulting.in>
2018-12-12 10:57:38 +0530

    nhance_cstm updates

--- nhance_cstm/nhance_cstm/report/summarizing_the_invoices/summarizing_the_invoices.py
+++ nhance_cstm/nhance_cstm/report/summarizing_the_invoices/summarizing_the_invoices.py
@@ -22,27 +22,16 @@ def execute(filters=None):
 	columns = get_columns()
 
 	data.append(["", "", ""])
-	invoice_list_for_day = get_day_wise_invoices()
 	data.append(["Day", "", ""])
-	if invoice_list_for_day is not None:
-		invoice_type = "Day"
-		get_invoice_amount(invoice_list_for_day,invoice_type)
-	
+	get_day_wise_invoices()
+
 	data.append(["", "", ""])
 	data.append(["Month To Date", "", ""])
-
-	invoice_list_for_month = get_month_wise_invoices()
-	if invoice_list_for_month is not None:
-		invoice_type = "Month"
-		get_invoice_amount(invoice_list_for_month,invoice_type)
+	get_month_wise_invoices()
 	
 	data.append(["", "", ""])
 	data.append(["Year To Date", "", ""])
-
-	invoice_list_for_fin_year = get_year_wise_invoices()
-	if invoice_list_for_fin_year is not None:
-		invoice_type = "Year"
-		get_invoice_amount(invoice_list_for_fin_year,invoice_type)
+	get_year_wise_invoices()
 	
 	return columns, data
 
@@ -54,67 +43,75 @@ def get_columns():
 	_("B-B")+"::150",
 	_("Total")+"::150"
 	 ]
-	return columns
-
-def get_invoice_amount(invoice_list,invoice_type):
-	customer_type = ""
-	total_invoice_amount_of_company = 0
-	total_invoice_amount_of_individual = 0
-	current_date = getdate(datetime.datetime.now())
-	c_date = frappe.utils.formatdate(current_date, "dd-MM-yyyy")
-	now = datetime.datetime.now()
-	current_year = now.year
-	monthinteger = now.month
-	current_month = datetime.date(1900, monthinteger, 1).strftime('%B')
-	for invoice_data in invoice_list:
-		customer_type = invoice_data['customer_type']
-		amount = invoice_data['net_total']
-		status = invoice_data['status']
-		if customer_type == "Company":
-			total_invoice_amount_of_company = total_invoice_amount_of_company + amount
-		elif customer_type == "Individual":
-			total_invoice_amount_of_individual = total_invoice_amount_of_individual + amount
-			print "total_invoice_amount_of_individual--------", total_invoice_amount_of_individual
-	
-	total = total_invoice_amount_of_company + total_invoice_amount_of_individual
-	if invoice_type == "Day":
-		data.append([str(c_date), total_invoice_amount_of_individual, total_invoice_amount_of_company, total])
-	elif invoice_type == "Month":
-		data.append([ (str(current_month) + ", " + str(current_year)), total_invoice_amount_of_individual, 				       total_invoice_amount_of_company, total])
-	elif invoice_type == "Year":
-		data.append([ ("FY" + str(current_year) + "-"+ str(current_year + 1)), total_invoice_amount_of_individual, 					total_invoice_amount_of_company, total])
+	return columns	
 
 def get_day_wise_invoices():
 	current_date = getdate(datetime.datetime.now())
-	invoice_details = frappe.db.sql("""select customer_type, name, net_total, status from `tabSales Invoice` where posting_date = %s and 
-					docstatus=1""", (current_date), as_dict=1)
-	if len(invoice_details)!=0:
-		return invoice_details
-	else:
-		return None
+	c_date = frappe.utils.formatdate(current_date, "dd-MM-yyyy")
+	individual_day = 0
+	company_day = 0
+	individual_day_key = frappe.db.sql("""select sum(net_total) as sum_net_total from `tabSales Invoice` where posting_date = %s and 
+					docstatus=1 and customer_type= "Individual" """, (current_date), as_dict=1)
+	if individual_day_key[0]["sum_net_total"]!= None:
+		individual_day = individual_day_key[0]["sum_net_total"]		
+
+	company_day_key = frappe.db.sql("""select sum(net_total) as sum_net_total from `tabSales Invoice` where posting_date = %s and 
+					docstatus=1 and customer_type= "Company" """, (current_date), as_dict=1)
+	if company_day_key[0]["sum_net_total"]!= None:
+		company_day = company_day_key[0]["sum_net_total"]
+
+	total_day = individual_day + company_day
+	
+	data.append([str(c_date), individual_day, company_day, total_day])	
+
 
 def get_month_wise_invoices():
 	d = date.today()
 	from_date = get_first_day(d)
 	to_date = getdate(datetime.datetime.now())
-	invoice_details = frappe.db.sql("""select customer_type, name, net_total, status from `tabSales Invoice` where posting_date >= %s and 
-					posting_date <= %s and docstatus=1""", (from_date, to_date), as_dict=1)
-	if len(invoice_details)!=0:
-		return invoice_details
-	else:
-		return None
+	now = datetime.datetime.now()
+	current_year = now.year
+	monthinteger = now.month
+	current_month = datetime.date(1900, monthinteger, 1).strftime('%B')
+
+	individual_month = 0
+	company_month = 0
+
+	individual_month_key = frappe.db.sql("""select sum(net_total) as sum_net_total from `tabSales Invoice` where posting_date >= %s and posting_date <= %s and docstatus=1 and customer_type= "Individual" """, (from_date, to_date), as_dict=1)
+	if individual_month_key[0]["sum_net_total"]!= None:
+		individual_month = individual_month_key[0]["sum_net_total"]	
+
+	company_month_key = frappe.db.sql("""select sum(net_total) as sum_net_total from `tabSales Invoice` where posting_date >= %s and posting_date <= %s and docstatus=1 and customer_type= "Company" """, (from_date, to_date), as_dict=1)
+	if company_month_key[0]["sum_net_total"]!= None:
+		company_month = company_month_key[0]["sum_net_total"]
+
+	total_month = individual_month + company_month
+
+	data.append([ (str(current_month) + ", " + str(current_year)), individual_month,company_month, total_month])
 
 def get_year_wise_invoices():
 	now = datetime.datetime.now()
 	current_year = now.year
 	from_date = str(current_year) + "-04-01"
 	to_date = getdate(datetime.datetime.now())
-	invoice_details = frappe.db.sql("""select customer_type, name, net_total, status from `tabSales Invoice` where posting_date >= %s and 
-					posting_date <= %s and docstatus=1""", (from_date, to_date), as_dict=1)
-	if len(invoice_details)!=0:
-		return invoice_details
-	else:
-		return None
+
+	individual_year = 0
+	company_year = 0
+
+	individual_year_key = frappe.db.sql("""select sum(net_total) as sum_net_total from `tabSales Invoice` where posting_date >= %s and 
+					posting_date <= %s and docstatus=1 and customer_type="Individual" """, (from_date, to_date), as_dict=1)
+	if individual_year_key[0]["sum_net_total"]!= None:
+		individual_year = individual_year_key[0]["sum_net_total"]
+
+	company_year_key = frappe.db.sql("""select sum(net_total) as sum_net_total from `tabSales Invoice` where posting_date >= %s and 
+					posting_date <= %s and docstatus=1 and customer_type="Company" """, (from_date, to_date), as_dict=1)
+	if company_year_key[0]["sum_net_total"]!= None:
+		company_year = company_year_key[0]["sum_net_total"]
+
+	total_year = individual_year + company_year
+
+	data.append([ ("FY" + str(current_year) + "-"+ str(current_year + 1)), individual_year,company_year, total_year])
+
 
 def get_first_day(dt, d_years=0, d_months=0):
     # d_years, d_months are "deltas" to apply to dt
